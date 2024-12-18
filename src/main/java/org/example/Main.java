12bfeda6ba45b067bc0bd44254d8cb466cf2ad4f
Kvitko Aleksandr <kvitok-993@mail.ru>
2024-12-18 16:26:36 +0300

    2 commit (Переворачиваем массив)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.example.home_tasks.Reverse;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Reverse reverse = new Reverse();
+        reverse.show();
     }
 }
