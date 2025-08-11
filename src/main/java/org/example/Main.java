89dfb04488e6ea548e6e755ffa872d81366399a2
kvitkoaleksandr <kvitok-993@mail.ru>
2025-08-12 00:15:17 +0300

    2 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,41 +15,5 @@ public class Main {
         names.forEach(out::println);
         out.println(names);
 
-        List<String> name = new ArrayList<>(Arrays.asList("John", "Paul", "Ringo", "George"));
-        names.forEach(n -> names.add("The Beatles"));
-        out.println(names);
-
-        String string = "Hello";
-
-        switch(string) {
-            case "hello":
-                out.println("hello tiny world");
-            default:
-                out.println("Good bye");
-                break;
-            case "Hello":
-                out.println("Hello Big World");
-        }
-        int[] array1 = {1, 2, 3, 4};
-        int[][] array2 = new int[][] {{1, 2, 3, 4}, {5, 6, 7, 8}};
-        int[][] array3 = {{1, 2}, {}};
-        int[] array4 = {};
-
-        out.println(array1.length); // 4
-        out.println(array2.length); // 2
-        out.println(array3.length); //2
-        out.println(array4.length);//0
-
-        int value = 4;
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-
-        list.stream()
-
-                .map(d -> d = value).forEach(out::println);
-
-        out.println(list);
     }
 }
