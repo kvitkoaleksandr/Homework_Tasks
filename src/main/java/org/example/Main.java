847c006082b7d513a8bd9798e6775711223ffdd5
kvitkoaleksandr <kvitok-993@mail.ru>
2025-08-12 09:24:03 +0300

    3 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,15 @@
 package org.example;
-import org.example.interviews.FizzBuzzTestInterview;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
 import static java.lang.System.out;
-import static java.util.stream.Collectors.toList;
 
 public class Main {
     public static void main(String[] args) {
         List<String> names = new ArrayList<>(Arrays.asList("John", "Paul", "Ringo", "George"));
         names.forEach(out::println);
         out.println(names);
-
     }
 }
